efbe8de81145d07893baba65faafca48e802c320
Rolandas Skrebutenas <rolandasskrebutenas@Rolandass-MBP.lan>
2023-01-09 19:55:22 +0200

    sect

--- src/Pages/addedCars.js
+++ src/Pages/addedCars.js
@@ -1,39 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
 import '../App.css';
-import HeaderLogo from '../images/zent-logo-png-car-22.png';
-import Header from '../Components/Header';
-import Footer from '../Components/Footer';
-import Addedcars from '../Pages/addedCars';
-import Addcar from '../Pages/addCar';
-import Button from '../Components/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <Header imgSrc={HeaderLogo} />
-      <nav className="nav">
-        <BrowserRouter>
-          <Button>
-            <Link to={'/Pages'}> Pasirinkti automobilius</Link>
-          </Button>
-
-          <Button>
-            <Link to={'/Pages'}> Ikelti automobili</Link>
-          </Button>
-
-          <Routes>
-            <Route path="/Pages" element={<Addedcars />} />
-            <Route path="/Pages" element={<Addcar />} />
-          </Routes>
-        </BrowserRouter>
-      </nav>
-      <div className="Hero">
-        <h1>Labas</h1>
-      </div>
-      <Footer />
-    </div>
-  );
+  return <h1>asd</h1>;
 }
 
 export default App;
