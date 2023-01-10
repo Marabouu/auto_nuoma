c5c246b18ee45fbc7a566c452769c34353e60d32
Rolandas Skrebutenas <rolandasskrebutenas@Rolandass-MBP.lan>
2023-01-10 19:33:45 +0200

    sect

--- src/Pages/Addcar/index.js
+++ src/Pages/Addcar/index.js
@@ -33,97 +33,57 @@ function App() {
     <div className="app">
       <p>Suveskite automobilio duomenys</p>
       <div className="app2">
-        <form className="form3">
+        <form className="form3" onSubmit={rere}>
           <div className="forms">
             <div className="form">
               <br />
-              <label for="brand">Automobilio marke</label>
+              <label htmlFor="brand">Automobilio marke</label>
               <br />
-              <input
-                type="text"
-                className="brand"
-                name="brand"
-                oninput="this.value = this.value.toUpperCase()"
-              />
+              <input type="text" className="brand" name="brand" />
               <br />
 
-              <label for="model">Modelis</label>
+              <label htmlFor="model">Modelis</label>
               <br />
-              <input
-                type="text"
-                className="model"
-                name="model"
-                oninput="this.value = this.value.toUpperCase()"
-              />
+              <input type="text" className="model" name="model" />
               <br />
 
-              <label for="year">Metai</label>
+              <label htmlFor="year">Metai</label>
               <br />
               <input type="text" className="year" name="year" />
               <br />
 
-              <label for="engine">Varyklio turis</label>
+              <label htmlFor="engine">Varyklio turis</label>
               <br />
-              <input
-                type="text"
-                className="engine"
-                name="engine"
-                oninput="this.value = this.value.toUpperCase()"
-              />
+              <input type="text" className="engine" name="engine" />
               <br />
 
-              <label for="engine">Pavaru Deze</label>
+              <label htmlFor="engine">Pavaru Deze</label>
               <br />
-              <input
-                type="text"
-                className="gearbox"
-                name="gearbox"
-                oninput="this.value = this.value.toUpperCase()"
-              />
+              <input type="text" className="gearbox" name="gearbox" />
               <br />
             </div>
             <div className="form2">
-              <label for="class">Tipas</label>
+              <label htmlFor="class">Tipas</label>
               <br />
-              <input
-                type="text"
-                className="class"
-                name="class"
-                oninput="this.value = this.value.toUpperCase()"
-              />
+              <input type="text" className="class" name="class" />
               <br />
 
-              <label for="carColor">Spalva</label>
+              <label htmlFor="carColor">Spalva</label>
               <br />
-              <input
-                type="text"
-                className="carColor"
-                name="carColor"
-                oninput="this.value = this.value.toUpperCase()"
-              />
+              <input type="text" className="carColor" name="carColor" />
               <br />
 
-              <label for="fuel">Kuro Tipas</label>
+              <label htmlFor="fuel">Kuro Tipas</label>
               <br />
-              <input
-                type="text"
-                className="fuel"
-                name="fuel"
-                oninput="this.value = this.value.toUpperCase()"
-              />
+              <input type="text" className="fuel" name="fuel" />
               <br />
 
-              <label for="mileage">Rida</label>
+              <label htmlFor="mileage">Rida</label>
               <br />
-              <input
-                type="text"
-                className="mileage"
-                name="mileage"
-                oninput="this.value = this.value.toUpperCase()"
-              />
+              <input type="text" className="mileage" name="mileage" />
               <br />
 
-              <label for="carImage">Nuotrauka (irasykit URL)</label>
+              <label htmlFor="carImage">Nuotrauka (irasykit URL)</label>
               <br />
               <input type="text" className="carImage" name="carImage" />
               <br />
